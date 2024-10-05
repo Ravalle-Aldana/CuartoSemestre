de67cc5e421b98993e3794054e348aee6e11138e
Aldana Ravalle <aldana.veronica.ravalle@gmail.com>
2024-10-04 22:57:08 -0300

    Guardar cambio al repositorio

---
+++ JAVASCRIPT/PERN-stack-05/src/controllers/tareas.controllers.js
@@ -0,0 +1,57 @@
+import { pool } from "../db.js";
+
+export const listarTareas = async (req, res) => {
+    const resultado = await pool.query("SELECT * FROM tareas");
+    console.log(resultado);
+    return res.json(resultado.rows)
+};
+
+export const listarTarea = async (req, res) => {
+    const resultado = await pool.query('SELECT * FROM tareas WHERE id = $1', [req.params.id]);
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: 'La tarea no existe'
+        });
+    }
+    return res.json(resultado.rows[0]);
+};
+
+export const crearTareas = async (req, res, next) => {
+    const { titulo, descripcion } = req.body;
+    try {
+        const result = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *', [titulo, descripcion]);
+        res.json(result.rows[0]);
+        console.log(result.rows[0]);
+    } catch (error) {
+        if (error.code === '23505') {
+            return res.status(400).send('¡Oh no!, ya existe una tarea con ese título');
+        }
+        console.log(error);
+        next(error);
+    }
+};
+
+export const actualizarTareas = async (req, res) => {
+    const { titulo, descripcion } = req.body;
+    const id  = req.params.id;
+    const result = await pool.query("UPDATE tareas SET titulo = $1, descripcion = $2 WHERE id = $3 Returning *", [titulo, descripcion, id]);
+
+    if (result.rowCount === 0) {
+        return res.status(404).json({
+            message: '¡Oh no!, la tarea no existe'
+        });
+    }
+
+    return res.json(result.rowCount[0]);
+};
+
+export const eliminarTareas = async (req, res) => {
+    const resultado = await pool.query('DELETE FROM tareas WHERE id = $1', [req.params.id]);
+
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: '¡Oh no!, la tarea no existe'
+        });
+    }
+    return res.sendStatus(204);
+};
